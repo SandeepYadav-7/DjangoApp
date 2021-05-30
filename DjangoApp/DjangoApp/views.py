94a7c060064a7a106cd552bfea9f71543cd2bfbf
SandeepYadav143 <sandeepyadav91577@gmail.com>
2021-05-30 20:00:27 +0530

    idk what should i write in commit lol

--- DjangoApp/DjangoApp/views.py
+++ DjangoApp/DjangoApp/views.py
@@ -4,8 +4,30 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 
-def about(request):
-	return render(request, 'about.html')
+def removePunc(text):
+	punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+	new = ""
+	for letter in text:
+		if letter in punctuations:
+			paas
+		else:
+			new += letter
+	print(new)
+	print("this function is working")
+	return new
 
 def utility(request):
+	user_text = request.GET.get("text", "Sandeep the coder")
+	check = request.GET.get("punc", "off")
+	# return HttpResponse(removePunc(user_text))
+	"""
+	if check == "on":
+		removePunc(user_text)
+		print("working this if statement")"""
+	removePunc(user_text)
+	print("this file is working")
+	print(f"\n{check}\n")
 	return render(request, 'utility.html')
+
+def about(request):
+	return render(request, 'about.html')
